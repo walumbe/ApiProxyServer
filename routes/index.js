201e00e6f441cb3b39aa01a17bbf240d25befbe0
walumbe <nathanwalumbe.com>
2021-11-18 18:50:09 +0300

    initial

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const apicache = require('apicache')
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
-console.log(API_KEY_VALUE);
+// console.log(API_KEY_VALUE);
 
 // Init cache
 let cache = apicache.middleware
